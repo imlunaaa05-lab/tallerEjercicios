cf079ef79eb565835096ee84b546708a0935b67b
Santiago Sarmiento <imlunaaa05@gmail.com>
2025-11-13 23:03:21 -0500

    Create perfecto_abundante_triangular.cpp

---
+++ Maths_And_Algorithms/perfecto_abundante_triangular.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool esTriangular(int n){
+    int suma = 0;
+    for(int i=1; suma<n; i++){
+        suma += i;
+        if(suma == n) return true;
+    }
+    return false;
+}
+
+int main(){
+    int n;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
+    int suma = 0;
+    for(int i=1; i<n; i++)
+        if(n % i == 0) suma += i;
+
+    if(suma == n) cout << "Perfecto" << endl;
+    else if(suma > n) cout << "Abundante" << endl;
+    else cout << "Deficiente" << endl;
+
+    if(esTriangular(n)) cout << "Ademas es triangular";
+}
