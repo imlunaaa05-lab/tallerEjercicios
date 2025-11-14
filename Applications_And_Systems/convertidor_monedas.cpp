33be51dda02ffaf07107168d77b9516d176beade
Santiago Sarmiento <imlunaaa05@gmail.com>
2025-11-13 23:39:19 -0500

    Create convertidor_monedas.cpp

---
+++ Applications_And_Systems/convertidor_monedas.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void convertidorMonedas() {
+    double cantidad;
+    int opcion;
+
+    const double tasaUSD = 4300.0; // ejemplo
+
+    cout << "\n--- CONVERTIDOR DE MONEDAS ---\n";
+    cout << "1. COP a USD\n";
+    cout << "2. USD a COP\n";
+    cout << "Seleccione: ";
+    cin >> opcion;
+
+    cout << "Ingrese cantidad: ";
+    cin >> cantidad;
+
+    if (opcion == 1)
+        cout << "USD: " << (cantidad / tasaUSD) << "\n";
+    else if (opcion == 2)
+        cout << "COP: " << (cantidad * tasaUSD) << "\n";
+    else
+        cout << "Opcion invalida.\n";
+}
