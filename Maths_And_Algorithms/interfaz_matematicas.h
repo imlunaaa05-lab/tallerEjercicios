63ebb9d9dcb7375abad95ff6b9b1e1e0b0692e3b
Santiago Sarmiento <imlunaaa05@gmail.com>
2025-11-13 23:10:28 -0500

    Create interfaz_matematicas.h

---
+++ Maths_And_Algorithms/interfaz_matematicas.h
@@ -0,0 +1,55 @@
+#ifndef INTERFAZ_MATEMATICAS_H
+#define INTERFAZ_MATEMATICAS_H
+
+
+void convertirDecimalABinario();
+
+
+void generarNumeroAleatorio();
+
+
+void calcularMCDyMCM();
+
+
+void calcularAreaTriangulo();
+
+
+void resolverEcuacionCuadratica();
+
+
+void ejecutarCollatz();
+
+
+void calcularVolumenEsfera();
+
+
+void estimarNumeroEuler();
+
+
+void evaluarPerfectoAbundanteTriangular();
+
+
+void evaluarFibonacciKaprekarNarcisistaMersenne();
+
+
+void aproximarPiLeibniz();
+
+
+void calcularRaicesNewton();
+
+
+void ejecutarBinomialYStirling();
+
+
+void aproximarLogaritmoTaylor();
+
+
+void generarFibonacciLucasPadovan();
+
+
+void sumarSeriesAritmeticasGeometricas();
+
+
+void sumarCuadrados();
+
+#endif
