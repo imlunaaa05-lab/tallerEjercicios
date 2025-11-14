7d04360696d5c64d495ff43c887701afa83ba607
Santiago Sarmiento <imlunaaa05@gmail.com>
2025-11-13 23:30:42 -0500

    Create interfaz_graficos.h

---
+++ Graphics_And_Patterns/interfaz_graficos.h
@@ -0,0 +1,22 @@
+#ifndef INTERFAZ_GRAFICOS_H
+#define INTERFAZ_GRAFICOS_H
+
+
+void imprimirPiramideAsteriscos();
+
+
+void imprimirPatronesVariados();
+
+
+void imprimirRectanguloHueco();
+
+
+void imprimirTableroAsteriscos();
+
+
+void imprimirPatronesEspeciales();
+
+
+void imprimirZigZagNumeros();
+
+#endif
