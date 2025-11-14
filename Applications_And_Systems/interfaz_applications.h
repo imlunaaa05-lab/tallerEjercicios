75bb1969463cdd8ad94869e3d653f344705802cb
Santiago Sarmiento <imlunaaa05@gmail.com>
2025-11-13 23:46:31 -0500

    Create interfaz_applications.h

---
+++ Applications_And_Systems/interfaz_applications.h
@@ -0,0 +1,31 @@
+#ifndef INTERFAZ_APPLICATIONS_H
+#define INTERFAZ_APPLICATIONS_H
+
+
+void calculadoraBasica();
+
+
+void sistemaReservaCine();
+
+
+void sistemaGestionNotas();
+
+
+void convertidorMonedas();
+
+
+void sistemaAutenticacion();
+
+
+void evaluadorExpresiones();
+
+
+void sistemaVotacion();
+
+
+void sistemaPuntajesJuego();
+
+
+void calculadoraCaloriasBMI();
+
+#endif
