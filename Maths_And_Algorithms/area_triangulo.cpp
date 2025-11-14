c7897dba80428ca8b04a057ae4a27eac7e8764b2
Santiago Sarmiento <imlunaaa05@gmail.com>
2025-11-13 23:01:04 -0500

    Create area_triangulo.cpp

---
+++ Maths_And_Algorithms/area_triangulo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    double a, b, c;
+    cout << "Lados del triangulo: ";
+    cin >> a >> b >> c;
+
+    double s = (a + b + c) / 2;
+    double area = sqrt(s * (s - a) * (s - b) * (s - c));
+
+    cout << "Area = " << area;
+}
