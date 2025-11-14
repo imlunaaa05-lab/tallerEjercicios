8dc2ecad19f78a268be9785f0e04fa2ecb0ed77f
Santiago Sarmiento <imlunaaa05@gmail.com>
2025-11-13 23:38:34 -0500

    Create gestion_notas.cpp

---
+++ Applications_And_Systems/gestion_notas.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void sistemaGestionNotas() {
+    int cantidad;
+    cout << "\n--- SISTEMA DE GESTION DE NOTAS ---\n";
+    cout << "Cantidad de estudiantes: ";
+    cin >> cantidad;
+
+    double suma = 0, nota;
+
+    for (int i = 1; i <= cantidad; i++) {
+        cout << "Nota del estudiante " << i << ": ";
+        cin >> nota;
+        suma += nota;
+    }
+
+    cout << "Promedio general: " << (suma / cantidad) << "\n";
+}
