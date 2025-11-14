667d6121541d15897f8a922b251f52124c2fbd24
Santiago Sarmiento <imlunaaa05@gmail.com>
2025-11-13 23:05:05 -0500

    Create taylor_logaritmos.cpp

---
+++ Maths_And_Algorithms/taylor_logaritmos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double x;
+    int terminos;
+    cout << "Ingrese x (0 < x <= 2): ";
+    cin >> x;
+    cout << "Cantidad de terminos: ";
+    cin >> terminos;
+
+    double resultado = 0;
+    double y = x - 1;
+
+    for(int n=1; n<=terminos; n++){
+        double termino = (n % 2 == 1 ? 1 : -1) * (pow(y, n) / n);
+        resultado += termino;
+    }
+
+    cout << "Logaritmo aproximado: " << resultado;
+}
