c22693aa65a89937122e0f86f256be973e945d7c
Santiago Sarmiento <imlunaaa05@gmail.com>
2025-11-13 23:24:19 -0500

    Create interfaz_simulacion.h

---
+++ Simulations_And_Games/interfaz_simulacion.h
@@ -0,0 +1,19 @@
+#ifndef INTERFAZ_SIMULACION_H
+#define INTERFAZ_SIMULACION_H
+
+
+void simularLanzamientoDado();
+
+
+void simularPiedraPapelTijera();
+
+
+void juegoAdivinarNumero();
+
+
+void generarTripletasPitagoricas();
+
+
+void generadorDistribucionesAleatorias();
+
+#endif
