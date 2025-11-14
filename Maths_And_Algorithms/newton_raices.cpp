13d563d017b4dd38b4dd8791076e14e9246b2370
Santiago Sarmiento <imlunaaa05@gmail.com>
2025-11-13 23:04:26 -0500

    Create newton_raices.cpp

---
+++ Maths_And_Algorithms/newton_raices.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+double raizCuadrada(double n){
+    double x = n;
+    for(int i=0; i<20; i++)
+        x = (x + n/x) / 2;
+    return x;
+}
+
+double raizCubica(double n){
+    double x = n;
+    for(int i=0; i<20; i++)
+        x = (2*x + n/(x*x)) / 3;
+    return x;
+}
+
+int main(){
+    double n;
+    cout << "Numero: ";
+    cin >> n;
+
+    cout << "Raiz cuadrada: " << raizCuadrada(n) << endl;
+    cout << "Raiz cubica: " << raizCubica(n) << endl;
+}
